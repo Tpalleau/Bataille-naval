8d866064ebaa7c54add61883e4069e26d0190eae
FlyCooper <85183889+FlyCooper@users.noreply.github.com>
2021-06-13 15:34:03 +0200

    Update save_files.h

--- save_files.h
+++ save_files.h
@@ -1,19 +1,17 @@
-#include <stido.h>
-#include <stdlib.h>
-
 int save_file() {
-    int i;
+    int boat_n;
     FILE* save = fopen("save>.txt","w");
     // checking if we can open the file
     if(save != NULL) {
-        for (i=0, i<5, ++i) { // write the coordinates of the boats, and separate them by a semi-colon
-            fprintf(save, "%d ;\n %d ;\n %d ;\n",boat.size[i],boat.direction[i],boat.spawn[i][2]);
+        for (boat_n=0; boat_n<5; ++boat_n) { // write the coordinates of the boats, and separate them by a semi-colon
+            fprintf(save, "%d ;\n %d ;\n %d ;\n %d ;\n",&boat.size[boat_n],&boat.direction[boat_n],&boat.spawn[boat_n][2],&boat.state[boat_n][4]);
         }
     }
     // still checking if we can open the file
     if(save != NULL) { // write the mode and difficulty, still separated by a semi-colon
-        fprintf(save, "%d ;\n %d ;\n", mode_choice, difficulty);
+        fprintf(save, "%d ;\n %d ;\n", game_mode, difficulty_mode);
     }
+    
     fclose(save); // close the file
     return 0;
 }
